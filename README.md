cc82f09d29af8c945bec4f4d5abb55ebf342b8e6
David Figatner <david@yopeyopey.com>
2016-09-17 22:41:20 +0800

    Updated to class/ES6; improved README documentation

--- README.md
+++ README.md
@@ -1,34 +1,135 @@
-## misc
-miscellaneous javascript functions.
+## misc.js
+miscellaneous javascript functions
 
-#### API
+## Installation
+include misc.js in your project or add to your workflow
 
-#### Misc.difference(a, b)
-returns difference between to numbers (i.e., larger - smaller)
+    npm install yy-misc
 
-#### Misc.arraySmallest(a)
+# API Reference
+**Kind**: global class  
+
+* [Misc](#Misc)
+    * [.difference(a, b)](#Misc+difference) ⇒ <code>number</code>
+    * [.arrayCount(a)](#Misc+arrayCount) ⇒ <code>object</code>
+    * [.arraySmallest(a)](#Misc+arraySmallest) ⇒ <code>number</code>
+    * [.sameSign(a, b)](#Misc+sameSign)
+    * [.preciseRound(num, decimals)](#Misc+preciseRound) ⇒ <code>number</code>
+    * [.wordCount(text)](#Misc+wordCount) ⇒ <code>number</code>
+    * [.aboutEqual(test, value, percent)](#Misc+aboutEqual) ⇒ <code>boolean</code>
+    * [.uniqueArray(a)](#Misc+uniqueArray) ⇒ <code>Array</code>
+    * [.clamp(n, min, max)](#Misc+clamp) ⇒ <code>number</code>
+
+<a name="Misc+difference"></a>
+
+### misc.difference(a, b) ⇒ <code>number</code>
+return the difference between two numbers (greater - lesser)
+
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
+
+| Param | Type |
+| --- | --- |
+| a | <code>number</code> | 
+| b | <code>number</code> | 
+
+<a name="Misc+arrayCount"></a>
+
+### misc.arrayCount(a) ⇒ <code>object</code>
+returns {most, least, all[]} array grouped by value
+
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
+**Returns**: <code>object</code> - {most: most, least: least, all: Array}  
+
+| Param | Type |
+| --- | --- |
+| a | <code>Array</code> | 
+
+<a name="Misc+arraySmallest"></a>
+
+### misc.arraySmallest(a) ⇒ <code>number</code>
 finds the smallest number in an array
 
-#### Misc.sign(x)
-returns the sign of a number (+1 or -1)
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
 
-#### Misc.preciseRound(num, decimals)
+| Param | Type |
+| --- | --- |
+| a | <code>Array</code> | 
+
+<a name="Misc+sameSign"></a>
+
+### misc.sameSign(a, b)
+returns whether a and b have the same sign or both equal 0
+
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
+
+| Param | Type |
+| --- | --- |
+| a | <code>number</code> | 
+| b | <code>number</code> | 
+|  | <code>boolean</code> | 
+
+<a name="Misc+preciseRound"></a>
+
+### misc.preciseRound(num, decimals) ⇒ <code>number</code>
 round number to certain number of decimals
 
-#### Misc.fontSize (text, maxWidth, maxHeight)
-returns a font-size based on a maxWidth or maxHeight
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
 
-#### Misc.wordCount(text)
-returns number of words
+| Param | Type |
+| --- | --- |
+| num | <code>number</code> | 
+| decimals | <code>number</code> | 
 
-#### Misc.aboutEqual(test, value, percent)
-Test whether the test is percent away from value
+<a name="Misc+wordCount"></a>
 
-#### Misc.uniqueArray(a)
+### misc.wordCount(text) ⇒ <code>number</code>
+from: http://stackoverflow.com/questions/6543917/count-number-of-words-in-string-using-javascript
+
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
+
+| Param | Type |
+| --- | --- |
+| text | <code>string</code> | 
+
+<a name="Misc+aboutEqual"></a>
+
+### misc.aboutEqual(test, value, percent) ⇒ <code>boolean</code>
+returns whether the test value is about equal to the value (i.e., within the given percentage)
+
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
+
+| Param | Type |
+| --- | --- |
+| test | <code>number</code> | 
+| value | <code>number</code> | 
+| percent | <code>number</code> | 
+
+<a name="Misc+uniqueArray"></a>
+
+### misc.uniqueArray(a) ⇒ <code>Array</code>
 Removes duplicates from array and returns new array
+from http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
 
-#### Misc.clamp(n, min, max)
-clamps a number between min and max
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
 
-## license
-MIT License (MIT)
+| Param | Type |
+| --- | --- |
+| a | <code>Array</code> | 
+
+<a name="Misc+clamp"></a>
+
+### misc.clamp(n, min, max) ⇒ <code>number</code>
+clamps a number
+
+**Kind**: instance method of <code>[Misc](#Misc)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| n | <code>number</code> | to clamp |
+| min | <code>number</code> |  |
+| max | <code>number</code> |  |
+
+
+* * *
+
+Copyright (c) 2016 YOPEY YOPEY LLC - MIT License - Documented by [jsdoc-to-markdown](https://github.com/75lb/jsdoc-to-markdown)
--- misc.js
+++ misc.js
@@ -5,170 +5,184 @@
     Copyright YOPEY YOPEY LLC
 */
 
-// return the difference between two numbers (greater - lesser)
-function difference(a, b)
+/** @class */
+class Misc
 {
-    return (a > b)? a - b : b - a;
-}
-
-// returns {most, least, all[]} about array grouped by value
-function arrayCount(a)
-{
-    function addIndex(index)
+    /**
+     * return the difference between two numbers (greater - lesser)
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    difference(a, b)
     {
-        for (var i = 0; i < result.length; i++)
+        return (a > b)? a - b : b - a;
+    }
+
+    /**
+     * returns {most, least, all[]} array grouped by value
+     * @param {Array} a
+     * @return {object} {most: most, least: least, all: Array}
+     */
+    arrayCount(a)
+    {
+        function addIndex(index)
         {
-            if (result[i].index === index)
+            for (let i = 0; i < result.length; i++)
             {
-                result[i].count++;
-                return;
+                if (result[i].index === index)
+                {
+                    result[i].count++;
+                    return;
+                }
+            }
+            result.push({index: index, count: 1});
+        }
+
+        const result = [];
+        for (let i = 0; i < a.length; i++)
+        {
+            addIndex(a[i]);
+        }
+        result.sort(
+            function(a, b)
+            {
+                return a.count - b.count;
+            }
+        );
+        return {most: result[result.length - 1].index, least: result[0].index, all: result};
+    }
+
+    /**
+     * finds the smallest number in an array
+     * @param {Array} a
+     * @return {number}
+     */
+    arraySmallest(a)
+    {
+        var smallest = -Infinity;
+        for (var i = 0; i < a.length; i++)
+        {
+            if (a[i] > smallest) smallest = a[i];
+        }
+        return smallest;
+    }
+
+    /**
+     * returns whether a and b have the same sign or both equal 0
+     * @param {number} a
+     * @param {number} b
+     * @param {boolean}
+     */
+    sameSign(a, b)
+    {
+        return (a < 0 && b < 0) || (a > 0 && b > 0) || (a === 0 && b === 0);
+    }
+
+    /**
+     * round number to certain number of decimals
+     * @param {number} num
+     * @param {number} decimals
+     * @return {number}
+     */
+    preciseRound(num, decimals)
+    {
+        var t = Math.pow(10, decimals);
+        return (Math.round((num * t) + (decimals > 0 ? 1 : 0) * (Math.sign(num) * (10 / Math.pow(100, decimals)))) / t).toFixed(decimals);
+    }
+
+    // adjusts font-size based on a maxWidth or maxHeight
+    fontSize (text, maxWidth, maxHeight)
+    {
+        if (!this.fontDummy)
+        {
+            this.fontDummy = document.createElement('div');
+            this.fontDummy.style.position = 'absolute';
+            this.fontDummy.style.float = 'left';
+            this.fontDummy.style.whiteSpace = 'no-wrap';
+            this.fontDummy.style.visibility = 'hidden';
+            this.fontDummy.style.fontFamily = 'Open Sans';
+            document.body.appendChild(this.fontDummy);
+        }
+        var size = 1;
+        this.fontDummy.style.fontSize = size + 'px';
+        this.fontDummy.innerHTML = text;
+        var count = 0;
+        while (count < 1000 && (!maxWidth || this.fontDummy.offsetWidth < maxWidth) && (!maxHeight || this.fontDummy.offsetHeight < maxHeight))
+        {
+            size++;
+            this.fontDummy.style.fontSize = size + 'px';
+            count++;
+        }
+        if (count === 1000)
+        {
+            console.error('Misc.fontSize not found.');
+        }
+        return size - 1;
+    }
+
+    /**
+     * from: http://stackoverflow.com/questions/6543917/count-number-of-words-in-string-using-javascript
+     * @param {string} text
+     * @return {number}
+     */
+    wordCount(text)
+    {
+        var result = text.match(/(\w+)/g);
+        return result ? result.length : 0;
+    }
+
+    /**
+     * returns whether the test value is about equal to the value (i.e., within the given percentage)
+     * @param {number} test
+     * @param {number} value
+     * @param {number} percent
+     * @return {boolean}
+     */
+    aboutEqual(test, value, percent)
+    {
+        var delta = value * percent;
+        return (test === value) || (test > value && test - delta <= value) || (test - delta >= value && test <= value);
+    }
+
+    /**
+     * Removes duplicates from array and returns new array
+     * from http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
+     * @param {Array} a
+     * @return {Array}
+     */
+    uniqueArray(a)
+    {
+        const seen = {};
+        const out = [];
+        const len = a.length;
+        let j = 0;
+        for (let i = 0; i < len; i++)
+        {
+            const item = a[i];
+            if (seen[item] !== 1)
+            {
+                seen[item] = 1;
+                out[j++] = item;
             }
         }
-        result.push({index: index, count: 1});
+        return out;
     }
 
-    var result = [];
-    for (var i = 0; i < a.length; i++)
+    /**
+     * clamps a number
+     * @param {number} n to clamp
+     * @param {number} min
+     * @param {number} max
+     * @return {number}
+     */
+    clamp(n, min, max)
     {
-        addToIndex(a[i]);
+        return Math.min(Math.max(n, min), max);
     }
-    result.sort(function(a, b) {
-        return a.count - b.count;
-    });
-    return {most: result[result.length - 1].index, least: result[0].index, all: result}
 }
 
-// finds the smallest number in an array
-function arraySmallest(a)
-{
-    var smallest = -Infinity;
-    for (var i = 0; i < a.length; i++)
-    {
-        if (a[i] > smallest) smallest = a[i];
-    }
-    return smallest;
-}
+module.exports = new Misc();
 
-function sign(x)
-{
-    return (x >= 0) ? 1 : -1;
-}
-
-// returns whether a and b have the same sign or both equal 0
-function sameSign(a, b)
-{
-    return (a < 0 && b < 0) || (a > 0 && b > 0) || (a === 0 && b === 0);
-}
-
-// round number to certain number of decimals
-function preciseRound(num, decimals)
-{
-    var t = Math.pow(10, decimals);
-    return (Math.round((num * t) + (decimals > 0 ? 1 : 0) * (Math.sign(num) * (10 / Math.pow(100, decimals)))) / t).toFixed(decimals);
-}
-
-// adjusts font-size based on a maxWidth or maxHeight
-var fontDummy;
-function fontSize (text, maxWidth, maxHeight)
-{
-    if (!fontDummy)
-    {
-        fontDummy = document.createElement("div");
-        fontDummy.style.position = "absolute";
-        fontDummy.style.float = "left";
-        fontDummy.style.whiteSpace = "no-wrap";
-        fontDummy.style.visibility = "hidden";
-        fontDummy.style.fontFamily = 'Open Sans';
-        document.body.appendChild(fontDummy);
-    }
-    var size = 1;
-    fontDummy.style.fontSize = size + "px";
-    fontDummy.innerHTML = text;
-    var count = 0;
-    while (count < 1000 && (!maxWidth || fontDummy.offsetWidth < maxWidth) && (!maxHeight || fontDummy.offsetHeight < maxHeight))
-    {
-        size++;
-        fontDummy.style.fontSize = size + "px";
-        count++;
-    }
-    if (count === 1000)
-    {
-        debug('Misc.fontSize not found.');
-    }
-    return size - 1;
-}
-
-// from: http://stackoverflow.com/questions/6543917/count-number-of-words-in-string-using-javascript
-function wordCount(text)
-{
-    var result = text.match(/(\w+)/g);
-    return result ? result.length : 0;
-}
-
-function aboutEqual(test, value, percent)
-{
-    var delta = value * percent;
-    return (test === value) || (test > value && test - delta <= value) || (test - delta >= value && test <= value);
-}
-
-// Removes duplicates from array and returns new array
-// from http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
-function uniqueArray(a)
-{
-    var seen = {};
-    var out = [];
-    var len = a.length;
-    var j = 0;
-    for (var i = 0; i < len; i++)
-    {
-        var item = a[i];
-        if (seen[item] !== 1)
-        {
-            seen[item] = 1;
-            out[j++] = item;
-        }
-    }
-    return out;
-}
-
-function clamp(n, min, max)
-{
-	return Math.min(Math.max(n, min), max);
-}
-
-var Misc = {
-    difference: difference,
-    arraySmallest: arraySmallest,
-    arrayCount: arrayCount,
-    sign: sign,
-    sameSign: sameSign,
-    preciseRound: preciseRound,
-    fontSize: fontSize,
-    wordCount: wordCount,
-    aboutEqual: aboutEqual,
-    uniqueArray: uniqueArray,
-    clamp: clamp
-};
-
-// add support for AMD (Asynchronous Module Definition) libraries such as require.js.
-if (typeof define === 'function' && define.amd)
-{
-    define(function()
-    {
-        return {
-            Misc: Misc
-        };
-    });
-}
-
-// add support for CommonJS libraries such as browserify.
-if (typeof exports !== 'undefined')
-{
-    module.exports = Misc;
-}
-
-// define globally in case AMD is not available or available but not used
-if (typeof window !== 'undefined')
-{
-    window.Misc = Misc;
-}
+// for eslint
+/* globals document, console */
